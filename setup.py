b45394b2fc41e65348fe969941c08fc6331e12e8
Ayushman KB <ayushmaan.banerjee@gmail.com>
2020-08-17 17:41:40 +0530

    Update setup.py
    Added author

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='QUBO2Ising',
       version='0.1.1',
-      description='Generate Ising model from a QUBO array.',
+      description='Generate Ising array from a QUBO array.',
       packages=['QUBO2Ising'],
-      author='Saptarshi Chakrabarti',
-      author_email='csaptarshi@pm.me',
-      project_urls={'GitHub': 'https://github.com/csap7'},
+      author='Saptarshi Chakrabarti, Ayushman Kumar Banerjee',
+      author_email='csaptarshi@pm.me, ayushmaan.banerjee@gmail.com',
+      project_urls={'GitHub': 'https://github.com/csap7/QUBO2Ising'},
       zip_safe=False
       )
